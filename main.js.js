ca7e22245a68c31898b126d609ec46a980f0a2be
Kelvin Ndungu <k.ndungu.waithera@gmail.com>
2026-07-28 15:55:56 +0300

    Delete main.js.js

--- main.js.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById('magic-button');
-const message = document.getElementById('message');
-
-button.addEventListener('click', () => {
-  message.textContent = 'The legendary lamp is glowing!';
-});
